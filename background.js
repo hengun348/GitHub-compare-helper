23603c090bc494cee6e4446c80e29ea0d1b9e412
Henrik Gunnarsson <hengun348@gmail.com>
2016-05-07 16:16:57 +0200

    refactor(background.js): change indentation to tabs

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { 
-                hostEquals: 'github.com', 
-                pathContains: 'commits'
-            },
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      } 
-    ]);
+	  
+	chrome.declarativeContent.onPageChanged.addRules([
+	  {
+		conditions: [
+		  new chrome.declarativeContent.PageStateMatcher({
+			pageUrl: { 
+				hostEquals: 'github.com', 
+				pathContains: 'commits'
+			},
+		  })
+		],
+		actions: [ new chrome.declarativeContent.ShowPageAction() ]
+	  } 
+	]);
   });
 });
