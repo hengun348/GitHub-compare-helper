ba6785f23e8a6f3243464e913464fbcbbce80360
Henrik Gunnarsson <hengun348@gmail.com>
2016-05-07 17:05:29 +0200

    feat(github compare helper): generate compare url on button click

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var commitListItems = Array.from(document.getElementsByClassName('commit'));
+
+
 addGenerateCompareUrlButton();
 addCheckboxes();
 
@@ -10,17 +13,43 @@ function addGenerateCompareUrlButton() {
 	
 	generateCompareUrlButton = document.createElement('button');
 	generateCompareUrlButton.id = 'generate-compare-url-button';
+	generateCompareUrlButton.onclick = generateCompareUrl;
 	
 	textNode = document.createTextNode('Generate compare url')
 	
 	generateCompareUrlButton.appendChild(textNode);
 	
 	selectMenu.appendChild(generateCompareUrlButton);
+	
+	function generateCompareUrl() {
+		var selectedCommits,
+			githubCompareUrl;
+			
+		selectedCommits = commitListItems
+				.map(toCheckboxes)
+				.filter(onSelectedCommits)
+				.map(toSHA);
+				
+		function toCheckboxes(listItem) {
+			return listItem.getElementsByClassName('commit-compare-checkbox')[0];
+		}
+		
+		function onSelectedCommits(checkbox) {
+			return checkbox.checked;
+		}
+		
+		function toSHA(checkbox) {
+			return checkbox.value;
+		}
+		
+		githubCompareUrl = 'https://github.com/reqtest/reqtest/compare/' +
+							selectedCommits[0] + '...' + selectedCommits[1];
+		
+		alert(githubCompareUrl);
+	}
 }
 
 function addCheckboxes() {
-	var commitListItems = Array.from(document.getElementsByClassName('commit'));
-
 	commitListItems.forEach(appendCheckbox);
 	
 	function appendCheckbox(listItem) {
