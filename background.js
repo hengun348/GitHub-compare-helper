1a8d8e0056a41da84d4235a3f9e199bb19350f69
Henrik Gunnarsson <hengun348@gmail.com>
2020-05-23 22:07:35 +0200

    only inject script in the tab that requires them
    error if users start loading page and quickly switches to other tab

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(tab.url.includes('/commits') && changeInfo.status === 'complete') {
 		if (tab.url.includes('/pull/')) {
-			injectPullRequestScripts();
+			injectPullRequestScripts(tabId);
 		} else {
-            injectProjectScripts();
+            injectProjectScripts(tabId);
         }
 	}
 });
 
-function injectProjectScripts() {
-	chrome.tabs.executeScript({file: "common.js"});
-	chrome.tabs.executeScript({file: "script-project.js"});
-	chrome.tabs.insertCSS({file: "styles.css"});
+function injectProjectScripts(tabId) {
+	chrome.tabs.executeScript(tabId, {file: "common.js"});
+	chrome.tabs.executeScript(tabId, {file: "script-project.js"});
+	chrome.tabs.insertCSS(tabId, {file: "styles.css"});
 }
 
-function injectPullRequestScripts() {
-	chrome.tabs.executeScript({file: "common.js"});
-	chrome.tabs.executeScript({file: "script-pr.js"});
-	chrome.tabs.insertCSS({file: "styles.css"});
+function injectPullRequestScripts(tabId) {
+	chrome.tabs.executeScript(tabId, {file: "common.js"});
+	chrome.tabs.executeScript(tabId, {file: "script-pr.js"});
+	chrome.tabs.insertCSS(tabId, {file: "styles.css"});
 }
